1cc6fd5620505e3b268556c053d8e240cb0326d0
daniel <danielane@qq.com>
2023-10-12 18:25:27 +0800

    init commit

---
+++ app/src/main/java/com/showyourfingers/lighter/presentation/layouts/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.showyourfingers.lighter.presentation.layouts
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import com.showyourfingers.lighter.R
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
